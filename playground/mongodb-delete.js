bd04031dd6886daba240b1c10a58f100b8eeaf5a
rurigrass <ruripg@gmail.com>
2018-09-02 21:51:58 +0100

    more deletes

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -15,7 +15,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     // DELETE ONE
     //    db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
     //         console.log(result);
-
     //    });
 
     //    // FIND ONE AND DELETE
@@ -23,13 +22,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //        console.log(result);
     //    })
 
+    // //FIND ONE DELETE SPECIFIC ID
     // db.collection('Users').findOneAndDelete({
     //     _id: new ObjectID('5b8c18f88676a7229b961271')
     // }).then((result) => {
     //     console.log(JSON.stringify(result, undefined, 2));  
     // });
 
-    db.collection('Users').deleteMany({name: 'Ruairidh'});
+    // //FIND ONE DELETE ALL WITH NAME RUAIRIDH
+    // db.collection('Users').deleteMany({name: 'Ruairidh'});
 
 
 
