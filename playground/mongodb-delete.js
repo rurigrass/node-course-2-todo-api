775bd27dcee2927448e0633c3743ae42e7bc5586
rurigrass <ruripg@gmail.com>
2018-09-02 21:41:09 +0100

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unnable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    //  DELETE MANY
+    //    db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+    //        console.log(result);
+    //    });
+
+    // DELETE ONE
+    //    db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
+    //         console.log(result);
+
+    //    });
+
+    //    // FIND ONE AND DELETE
+    //    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //        console.log(result);
+    //    })
+
+    // db.collection('Users').findOneAndDelete({
+    //     _id: new ObjectID('5b8c18f88676a7229b961271')
+    // }).then((result) => {
+    //     console.log(JSON.stringify(result, undefined, 2));  
+    // });
+
+    db.collection('Users').deleteMany({name: 'Ruairidh'});
+
+
+
+
+    // db.close();
+});
--- playground/wongle.js
+++ playground/wongle.js
@@ -7,17 +7,56 @@ MongoClient.connect('mongodb://localhost:27017/WongleApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('WongleApp')
 
-    db.collection('Wongles').insertOne({
-        Supplier: 'New Co Ltd',
-        Product: 'Small wongle',
-        Price: 5
-    }, (err, result) =>{
+    db.collection('Wongles').insertMany([
+        {
+            idNumber: 0,
+            supplier: 'New Co Ltd',
+            product: 'Small wongle',
+            price: 5
+        },
+        {
+            idNumber: 1,
+            supplier: 'New Co Ltd',
+            product: 'Large wongle',
+            price: 8
+        },
+        {
+            idNumber: 2,
+            supplier: 'New Co Ltd',
+            product: 'Super wongle',
+            price: 12
+        },
+        {
+            idNumber: 3,
+            supplier: 'Old Co Ltd',
+            product: 'Mini wongle',
+            price: 4
+        },
+        {
+            idNumber: 4,
+            supplier: 'Old Co Ltd',
+            product: 'Small wongle',
+            price: 6
+        },
+        {
+            idNumber: 5,
+            supplier: 'Old Co Ltd',
+            product: 'Large wongle',
+            price: 9
+        },
+        {
+            idNumber: 6,
+            supplier: 'Old Co Ltd',
+            product: 'Super wongle',
+            price: 13
+        }
+    ], (err, result) => {
         if (err) {
             return console.log('Unable to insert wongle', err);
         }
 
         console.log(JSON.stringify(result.ops, undefined, 2));
     });
-    
+
     client.close();
 });
